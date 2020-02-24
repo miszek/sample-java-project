3f891d17500d5e70fef9b16fb7b206ec70b1316f
miszek <szekalski.michal@gmail.com>
2020-02-24 21:56:49 +0100

    commit prior to git push origin master command check

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
-//comment added to check git pull origin master command
+/* comment added to check git pull origin master command */
+//comment added to check git push origin master command
 /**
  * The main class of the application. It contains the main() method,
  * the first method called.
